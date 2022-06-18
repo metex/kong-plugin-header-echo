1120f08daad2b4f117b679191fc9d397dde1e989
Algore <bruno.mike.gomes@gmail.com>
2022-06-18 14:58:03 +0100

    Update handler.lua

--- kong/plugins/kong-plugin-header-echo/handler.lua
+++ kong/plugins/kong-plugin-header-echo/handler.lua
@@ -1,39 +1,56 @@
-local BasePlugin = require "kong.plugins.base_plugin"
+local CustomHandler = {
+  VERSION  = "0.1.0",
+  PRIORITY = 2000,
+}
 
-local EchoHandler = BasePlugin:extend()
-local cache = kong.cache
-
-
-EchoHandler.PRIORITY = 2000
-EchoHandler.VERSION = "0.1.0"
-
-function EchoHandler:new()
-  EchoHandler.super.new(self, "kong-plugin-header-echo")
-
+function CustomHandler:init_worker()
+  -- Implement logic for the init_worker phase here (http/stream)
+  kong.log("init_worker")
   self.echo_string = ""
-
-  -- kong.log("In EchoHandler:new(). self.echo_string is: " .. self.echo_string)
-  kong.log("In EchoHandler:new(). self.echo_string is: ")
 end
 
-function EchoHandler:access(conf)
-  EchoHandler.super.access(self)
 
-  -- kong.log("In EchoHandler:accept(). self.echo_string is: " .. self.echo_string)
-  kong.log("In EchoHandler:accept(). self.echo_string is: ")
+function CustomHandler:preread(config)
+  -- Implement logic for the preread phase here (stream)
+  kong.log("preread")
+end
 
+
+function CustomHandler:certificate(config)
+  -- Implement logic for the certificate phase here (http/stream)
+  kong.log("certificate")
+end
+
+function CustomHandler:rewrite(config)
+  -- Implement logic for the rewrite phase here (http)
+  kong.log("rewrite")
+end
+
+function CustomHandler:access(config)
+  -- Implement logic for the rewrite phase here (http)
+  kong.log("access")
   self.echo_string = kong.request.get_header(conf.requestHeader)
 end
 
-function EchoHandler:header_filter(conf)
-  EchoHandler.super.header_filter(self)
-
-  -- kong.log("In EchoHandler:header_filter(). self.echo_string is: " .. self.echo_string)
+function CustomHandler:header_filter(config)
+  -- Implement logic for the header_filter phase here (http)
+  kong.log("header_filter")
   kong.log("In EchoHandler:header_filter(). self.echo_string is: ")
 
-  if self.echo_string ~= "" then
+  if self.echo_string ~= nil and self.echo_string ~= "" then
     kong.response.set_header(conf.responseHeader, self.echo_string)
   end
 end
 
-return EchoHandler
+function CustomHandler:body_filter(config)
+  -- Implement logic for the body_filter phase here (http)
+  kong.log("body_filter")
+end
+
+function CustomHandler:log(config)
+  -- Implement logic for the log phase here (http/stream)
+  kong.log("log")
+end
+
+-- return the created table, so that Kong can execute it
+return CustomHandler
